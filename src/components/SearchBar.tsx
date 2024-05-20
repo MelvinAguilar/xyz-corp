ec0b14e77d03a2e0560f6790ec2e25f23d5e6738
Melvin Aguilar <90595158+MelvinAguilar@users.noreply.github.com>
2024-05-20 06:21:43 +0000

    :bug: Remove unused code

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { SearchIcon } from "./Icons";
 import { formUrlQuery, removeKeysFromQuery } from "../lib/utils";
